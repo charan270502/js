c0565e6e95992778e173e5ce2e616efccc3c0e4c
SANGEE <murugancharan245@gmail.com>
2021-08-22 14:04:20 +0530

    basic

---
+++ varbiles.js
@@ -0,0 +1,42 @@
+//string
+
+var fullName = "Charan Murugan";
+
+//Numbers
+
+var age = 19;
+
+//Boolean
+
+var voteEligible = true;
+
+//variable declaration
+
+var nationality;
+
+//variable initialization
+
+nationality = "INDIAN";
+
+// const declaration is fixed
+
+/*let can be changed as var ,
+but let is blockScope ,
+where var is functionScope
+*/
+var firstName = "Charan";
+var lastName = "Murugan";
+var age = 18;
+var country = "India";
+var userName = "Charanmurugan@123";
+var password = "Qwerty@1234";
+var confirmPassword = "Qwerty@1234";
+
+console.log(`
+  Full Name is : ${firstName} ${lastName}
+  My Age is : ${age}
+  Country : ${country}
+  UserName : ${userName}
+  Password : ${password}
+
+`);
