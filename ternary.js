5e5183d94df04cafb71be44f0bddae024ff66df9
SANGEE <murugancharan245@gmail.com>
2021-08-22 15:51:31 +0530

    ternary

---
+++ ternary.js
@@ -0,0 +1,17 @@
+var age = 19;
+
+var result = console.log(
+  age > 18 ? "eligible to vote" : "not eligible to vote"
+);
+
+var aunthenticated = false;
+
+// if (aunthenticated) {
+//   console.log("Sign Out");
+// } else {
+//   console.log("Signup for free");
+// }
+
+//ternary operator
+
+console.log(aunthenticated ? "Sign Out" : "Signup for freely");
