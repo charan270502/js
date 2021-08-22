502b66c241cbead577805bf163da7d4e79ed08f1
SANGEE <murugancharan245@gmail.com>
2021-08-22 14:30:54 +0530

    operator

---
+++ operator.js
@@ -0,0 +1,19 @@
+//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
+
+var num1 = 23;
+var num2 = 67;
+
+console.log(`
+
+  Addition : ${num1 + num2}
+  Subtraction :${num1 - num2}
+  Division :${num1 / num2}
+  Multiplication :${num1 * num2}
+
+`);
+// discount calculator
+var fixedPrice = "45673";
+var discountPrice = "23433";
+var discount = ((fixedPrice - discountPrice) / fixedPrice) * 100;
+
+console.log(`the discount % : ${discount.toFixed(2)} %`);
