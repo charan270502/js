a397185ca1a0d93d6d7bfa9cf82149a1af77c0cf
SANGEE <murugancharan245@gmail.com>
2021-08-22 15:26:22 +0530

    conditional

---
+++ conditional.js
@@ -0,0 +1,16 @@
+var votingAge = 18;
+var myAge = 19;
+
+if (myAge >= votingAge) {
+  console.log("vote to eligible");
+} else {
+  console.log("not eligible to vote");
+}
+
+var google = true,
+  faacebook = false,
+  email = true;
+
+if (google || faacebook || email) {
+  console.log("Succesfully logged in");
+}
