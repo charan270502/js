428c68c5458d73593385abee62e56102c2c487b4
SANGEE <murugancharan245@gmail.com>
2021-08-25 15:38:24 +0530

    splice and slice

---
+++ sliceandsplice.js
@@ -0,0 +1,7 @@
+var days = ["mon", "tue", "wed", "thurs", "fri", "sat"];
+
+console.log(days);
+
+console.log(days.slice(days.length - 1));
+var result = days.splice(3, 0, "charan", "murugan");
+console.log(days);
